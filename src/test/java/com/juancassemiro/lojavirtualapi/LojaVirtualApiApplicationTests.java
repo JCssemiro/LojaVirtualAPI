d60a02a65ea431a580cb528f2903db4a5235f5f7
Juan Cassemiro <juancssemiro@gmail.com>
2024-03-14 14:16:05 -0400

    first commit

---
+++ src/test/java/com/juancassemiro/lojavirtualapi/LojaVirtualApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juancassemiro.lojavirtualapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
